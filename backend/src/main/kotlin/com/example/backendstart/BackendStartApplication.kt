b97ea3e8bc4924fde0cde6e7eb6d0fbcf00dfd0b
stefhol <hoef.stef@protonmail.ch>
2023-07-29 15:58:28 +0200

    Backend Coolio 🤪

---
+++ backend/src/main/kotlin/com/example/backendstart/BackendStartApplication.kt
@@ -0,0 +1,11 @@
+package com.example.backendstart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendStartApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendStartApplication>(*args)
+}
